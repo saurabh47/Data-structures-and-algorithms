213eb4283a9c1f980cf6428afbb45f90e2d38bf4
saurabh47 <saurabhgangamwar78@gmail.com>
2018-08-30 11:10:03 +0530

    Create linear.c

---
+++ Linear/linear.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc,char *argv[]){
+	if(argc==1)
+		return 0;	
+	int n = atoi(argv[1]);
+	int i=2;
+	while(i<argc){
+		if(n==atoi(argv[i])){
+			printf("Number Found at %d\n",i-1);	
+			return 0;
+			//break;
+		}		
+	i++;	
+	}		
+	printf("Number Not found\n");
+
+return 0;
+}

0a6d3be71baeb8e6be11bba433478c4be71a5672
mahesh jamdade <maheshmn121@gmail.com>
2022-08-31 16:12:17 -0400

    format file

--- data_structures/Binary Tree Traversal/preorder-inorder-postorder.c
+++ data_structures/Binary Tree Traversal/preorder-inorder-postorder.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<malloc.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -11,91 +11,96 @@ struct node
 
 struct node *tree;
 void create_tree(struct node *);
-struct node *insertElement(struct node *,int);
+struct node *insertElement(struct node *, int);
 void preorderTraversal(struct node *);
 void inorderTraversal(struct node *);
 void postorderTraversal(struct node *);
-int searchElement(struct node *,int);
+int searchElement(struct node *, int);
 void main()
 {
-int option,val;
-struct node *ptr;
-create_tree(tree);
-do
-{
-printf("\n1.Insert element\n2.Preorder traversal\n3.Inorder traversal\n4.Postorder traversal\n5.Search a element\n6.EXIT");
-printf("\nEnter an option: ");
-scanf("%d",&option);
-switch(option)
-{
-case 1:printf("\nEnter the value of the new node: ");
-scanf("%d",&val);
-tree=insertElement(tree,val);
-break;
+    int option, val;
+    struct node *ptr;
+    create_tree(tree);
+    do
+    {
+        printf("\n1.Insert element\n2.Preorder traversal\n3.Inorder traversal\n4.Postorder traversal\n5.Search a element\n6.EXIT");
+        printf("\nEnter an option: ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            printf("\nEnter the value of the new node: ");
+            scanf("%d", &val);
+            tree = insertElement(tree, val);
+            break;
 
-case 2:printf("\nThe elements of the tree: ");
-preorderTraversal(tree);
-break;
+        case 2:
+            printf("\nThe elements of the tree: ");
+            preorderTraversal(tree);
+            break;
 
-case 3:printf("\nThe elements of the tree: ");
-inorderTraversal(tree);
-break;
+        case 3:
+            printf("\nThe elements of the tree: ");
+            inorderTraversal(tree);
+            break;
 
-case 4:printf("\nThe elements of the tree: ");
-postorderTraversal(tree);
-break;
+        case 4:
+            printf("\nThe elements of the tree: ");
+            postorderTraversal(tree);
+            break;
 
-case 5:printf("\nEnter the value to be searched: ");
-scanf("%d",&val);
-searchElement(tree,val);
-break;
-}
-}while(option!=6);
+        case 5:
+            printf("\nEnter the value to be searched: ");
+            scanf("%d", &val);
+            searchElement(tree, val);
+            break;
+        }
+    } while (option != 6);
 }
 
 void create_tree(struct node *tree)
 {
-tree=NULL;
+    tree = NULL;
 }
 
-struct node *insertElement(struct node *tree,int val)
+struct node *insertElement(struct node *tree, int val)
 {
-    struct node *ptr,*nodeptr,*parentptr;
-    ptr=(struct node *)malloc(sizeof(struct node));
-    ptr->data=val;
-    ptr->left=NULL;
-    ptr->right=NULL;
-    if(tree==NULL)
+    struct node *ptr, *nodeptr, *parentptr;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = val;
+    ptr->left = NULL;
+    ptr->right = NULL;
+    if (tree == NULL)
     {
-        tree=ptr;
-        tree->left=NULL;
-        tree->right=NULL;
+        tree = ptr;
+        tree->left = NULL;
+        tree->right = NULL;
     }
     else
     {
-        parentptr=NULL;
-        nodeptr=tree;
-        while(nodeptr!=NULL)
+        parentptr = NULL;
+        nodeptr = tree;
+        while (nodeptr != NULL)
         {
-            parentptr=nodeptr;
-            if(val<nodeptr->data)
-            nodeptr=nodeptr->left;
+            parentptr = nodeptr;
+            if (val < nodeptr->data)
+                nodeptr = nodeptr->left;
             else
-            nodeptr=nodeptr->right;
+                nodeptr = nodeptr->right;
         }
-        if(val<parentptr->data)
-        parentptr->left=ptr;
+        if (val < parentptr->data)
+            parentptr->left = ptr;
         else
-        parentptr->right=ptr;
+            parentptr->right = ptr;
     }
     return tree;
 }
 
 void preorderTraversal(struct node *tree)
+{
+    if (tree != NULL)
     {
-    if(tree!=NULL)
-    {
-        printf("%d\t",tree->data);
+        printf("%d\t", tree->data);
         preorderTraversal(tree->left);
         preorderTraversal(tree->right);
     }
@@ -103,34 +108,32 @@ void preorderTraversal(struct node *tree)
 
 void inorderTraversal(struct node *tree)
 {
-    if(tree!=NULL)
+    if (tree != NULL)
     {
         inorderTraversal(tree->left);
-        printf("%d\t",tree->data);
+        printf("%d\t", tree->data);
         inorderTraversal(tree->right);
     }
 }
 
-
 void postorderTraversal(struct node *tree)
 {
-    if(tree!=NULL)
+    if (tree != NULL)
     {
         postorderTraversal(tree->left);
         postorderTraversal(tree->right);
-        printf("%d\t",tree->data);
+        printf("%d\t", tree->data);
     }
 }
 
-int searchElement(struct node *tree,int val)
+int searchElement(struct node *tree, int val)
 {
-    if(tree==NULL)
-    printf("\nElement not  found");
-    else if(tree->data==val)
-    printf("\nElement found");
-    else if(val<tree->data)
-    return searchElement(tree->left,val);
-    else if(val>tree->data)
-    return searchElement(tree->right,val);
+    if (tree == NULL)
+        printf("\nElement not  found");
+    else if (tree->data == val)
+        printf("\nElement found");
+    else if (val < tree->data)
+        return searchElement(tree->left, val);
+    else if (val > tree->data)
+        return searchElement(tree->right, val);
 }
-
--- hackerrank/clock_format.cpp
+++ hackerrank/clock_format.cpp
@@ -9,8 +9,7 @@
  * Copyright 2022 - 2022 Widget Media Labs
  */
 
-
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -23,52 +22,72 @@ using namespace std;
 
 string toStr(int x);
 
-string timeConversion(string s) {
- int hours,minutes,seconds=0;
- hours = stoi(s.substr(0,2));
- minutes = stoi(s.substr(3,2));
- seconds = stoi(s.substr(6,2));
- string meridian = s.substr(8,2);
-// cout<<"hours="<<hours<<"minutes="<<minutes<<"seconds="<<seconds<<endl;
- if(meridian.compare("AM")==0){
-    if(hours<12){
-        return toStr(hours)+":"+toStr(minutes)+":"+toStr(seconds);        
-    }else if(hours==12){
-        if(minutes==0){
-            if(seconds==0){
-                return "00:00:00";
-            }else{
-                hours=0;
-                return toStr(hours)+":"+toStr(minutes)+":"+toStr(seconds);   
+string timeConversion(string s)
+{
+    int hours, minutes, seconds = 0;
+    hours = stoi(s.substr(0, 2));
+    minutes = stoi(s.substr(3, 2));
+    seconds = stoi(s.substr(6, 2));
+    string meridian = s.substr(8, 2);
+    // cout<<"hours="<<hours<<"minutes="<<minutes<<"seconds="<<seconds<<endl;
+    if (meridian.compare("AM") == 0)
+    {
+        if (hours < 12)
+        {
+            return toStr(hours) + ":" + toStr(minutes) + ":" + toStr(seconds);
+        }
+        else if (hours == 12)
+        {
+            if (minutes == 0)
+            {
+                if (seconds == 0)
+                {
+                    return "00:00:00";
+                }
+                else
+                {
+                    hours = 0;
+                    return toStr(hours) + ":" + toStr(minutes) + ":" + toStr(seconds);
+                }
             }
-        }else{
-            hours=0;
-            return toStr(hours)+":"+toStr(minutes)+":"+toStr(seconds);   
+            else
+            {
+                hours = 0;
+                return toStr(hours) + ":" + toStr(minutes) + ":" + toStr(seconds);
+            }
         }
     }
- }else{
-     if(hours<12){
-         hours+=12;
-            return toStr(hours)+":"+toStr(minutes)+":"+toStr(seconds);   
-     }else{
-            return toStr(hours)+":"+toStr(minutes)+":"+toStr(seconds);   
-     }
- }
- return s; 
+    else
+    {
+        if (hours < 12)
+        {
+            hours += 12;
+            return toStr(hours) + ":" + toStr(minutes) + ":" + toStr(seconds);
+        }
+        else
+        {
+            return toStr(hours) + ":" + toStr(minutes) + ":" + toStr(seconds);
+        }
+    }
+    return s;
 }
 
-string toStr(int x){
-    if(x<10){
-      return  "0"+to_string(x);
-    }else{
-      return to_string(x);
+string toStr(int x)
+{
+    if (x < 10)
+    {
+        return "0" + to_string(x);
+    }
+    else
+    {
+        return to_string(x);
     }
 }
 
 int main()
 {
 
-    string s="12:01:00AM";
+    string s = "12:01:00AM";
 
     string result = timeConversion(s);
 

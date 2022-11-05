9ee9c1072ca922f662491977c7efe1d916fdc65c
mahesh jamdade <maheshmn121@gmail.com>
2022-11-05 14:15:49 -0400

    adjacency list for un/directed graph

--- data_structures/graphs/adjacency_list.py
+++ data_structures/graphs/adjacency_list.py
@@ -1,67 +1,62 @@
-# A class to represent the structure of a node in the graph
-class Node:
-  def __init__(self, data):
-    self.vertex = data
-    self.next = None
-
 # A class to represent a graph. A graph is the list of the adjacency lists.
 # Size of the array will be the no of the vertices "nodes"
 
 class Graph:
-  def __init__(self, size, nodes):
+  def __init__(self, size, directed):
     self.nodes_len = size
-    self.nodes = nodes
-    self.roots = [None] * self.nodes_len
+    self.roots = {}
+    self.directed = directed
 
   # connects node a and b
-  def addNode(self, a, b):
-    index_a= self.nodes.index(a)
-    index_b= self.nodes.index(b)
-    node_a = Node(a)
-    node_a.next = self.roots[index_b]
-    self.roots[index_b] = node_a
+  def addEdge(self, a, b):
+    # index_a = self.nodes.index(a)
+    # index_b = self.nodes.index(b)
+    if a not in self.roots:
+      self.roots[a] = []
+    if b not in self.roots:
+      self.roots[b] = []
 
-    node_b = Node(b)
-    node_b.next = self.roots[index_a]
-    self.roots[index_a] = node_b
+    self.roots[a].append(b)
+    if self.directed == True:
+      self.roots[b].append(a)
 
   def showGraph(self):
-    for index in range(self.nodes_len):
-      print("Current Vertex at: {}".format(self.nodes[index]))
-      root = self.roots[index]
-      while (root):
-        print("Vertex {}".format(root.vertex))
-        root = root.next
+    for key, values in self.roots.items():
+      print("Current Vertex at: {}".format(key))
+      for i in range(len(values)):
+        print("Vertex {}".format(values[i]))
 
 #
-# 0 - - 1
+# 3 - - 4
+# |    /
+# |   /
 # |  /
 # | /
-# 2 - - 3
+#  2 -- -- 3
 #
-if __name__ == "__main__":
-  graph = Graph(4, [3, 4, 7, 8])
 
-  graph.addNode(3, 4)
-  graph.addNode(3, 7)
-  graph.addNode(4, 7)
-  graph.addNode(7, 8)
+if __name__ == "__main__":
+  # graph with 4 nodes and directed = False
+  graph = Graph(4, False)
+  graph.addEdge(3, 4)
+  graph.addEdge(3, 7)
+  graph.addEdge(4, 7)
+  graph.addEdge(7, 8)
   print(graph.showGraph())
 
+### Output ###
 
-
-### output
-# mahesh@Maheshs-MacBook-Air-M1 graphs % python3 adjacency_list.py
 # Current Vertex at: 3
-# Vertex 7
 # Vertex 4
+# Vertex 7
 # Current Vertex at: 4
+# Vertex 3
 # Vertex 7
-# Vertex 3
 # Current Vertex at: 7
-# Vertex 8
-# Vertex 4
 # Vertex 3
+# Vertex 4
+# Vertex 8
 # Current Vertex at: 8
 # Vertex 7
 # None
+# 

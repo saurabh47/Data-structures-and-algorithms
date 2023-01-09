ef8244f68031f1a24cfc7f26497a31c947a2c133
mahesh jamdade <maheshmn121@gmail.com>
2023-01-08 23:40:54 -0500

    add a comment

--- leetcode/problem_128.py
+++ leetcode/problem_128.py
@@ -2,16 +2,19 @@
 
 class Solution:
     def longestConsecutive(self, nums) -> int:
+        # Alternatively we can convert to set
+        # and remove the duplicate check
         sortedlist = sorted(nums)
         start = 0
         result = []
         temp = []
-        print(sortedlist)
         for end in range(len(sortedlist)):
             item = sortedlist[end]
             if(start != end):
                 if(abs(item - sortedlist[end-1]) == 1):
                     temp.append(item)
+                # Skip duplicate numbers
+                # e.g 0 1 1 2
                 elif(abs(item - sortedlist[end-1]) == 0):
                     continue
                 else:

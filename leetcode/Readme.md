76e786c55300689f80c6852be324e82ec8e0502a
mahesh jamdade <maheshmn121@gmail.com>
2023-01-14 22:18:30 -0500

    problem 53 with 3 approaches

--- leetcode/Readme.md
+++ leetcode/Readme.md
@@ -104,6 +104,19 @@ Explanation: The groupings are as follows:
 "bat" is not an anagram of any other word, so it its own group.
 ```
 
+### [Problem 53. Maximum Subarray](https://leetcode.com/problems/maximum-subarray/)
+
+Solved with 3 different approaches
+1. Brute Force
+2. Divide and Conquer
+3. Kadane's Algorithm
+
+```
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+```
+
 ### [Problem 125. Valid Palindrome](https://leetcode.com/problems/valid-palindrome/)
 
 ```
---
+++ leetcode/problem_53.py
@@ -0,0 +1,87 @@
+# Problem 53: Maximum Subarray (Medium): https://leetcode.com/problems/maximum-subarray/ 
+
+### Brute Force Solution
+# Time Complexity: O(n^3)
+class Solution1:
+    def maxSubArray(self, nums):
+        result = {}
+        max_sum = -pow(10,4)
+        length = len(nums)
+        for size in range(1, length+1):
+            if(size not in result):
+                result[size] = []
+            for i in range(length+1-size):
+                temp = []
+                start = i
+                for j in range(i,i+size):
+                    temp.append(nums[j])
+                temp_sum = sum(temp)
+                if(temp_sum > max_sum):
+                    max_sum = temp_sum
+                result[size].append(temp)
+        return max_sum
+
+# Brute Force Solution with Optimization
+# Time Complexity: O(n^2)
+class Solution2:
+    def maxSubArray(self, nums):
+        max_sum = -pow(10,4)
+        for i in range(1, len(nums)):
+            arr_sum = 0
+            for j in range(len(nums)+1):
+                if(i+j>len(nums)):
+                    break
+                arr_sum += nums[i+j-1]
+                max_sum = max(max_sum, arr_sum)
+                print('arr_sum: ', arr_sum, 'max_sum: ', max_sum)
+        return max_sum
+
+# Divide and Conquer Solution
+# Time Complexity: O(nlogn)
+class Solution3:
+    def maxSubArray(self, nums):
+        max_sum = -pow(10,4)
+        length = len(nums)
+        if(length==1):
+            return nums[0]
+        mid = length//2
+        # print("mid=",mid)
+        max_left = self.maxSubArray(nums[:mid])
+        max_right = self.maxSubArray(nums[mid:])
+        left_sum = -pow(10,4)
+        temp_sum = 0
+        for i in range(mid-1,-1,-1):
+            temp_sum += nums[i]
+            left_sum = max(left_sum,temp_sum)
+        temp_sum = 0
+        right_sum = -pow(10,4)
+        for i in range(mid,length):
+            temp_sum += nums[i]
+            right_sum = max(right_sum,temp_sum)
+        max_sum = max(max(max_left,max_right),left_sum+right_sum)
+        return max_sum
+
+# Kadane's Algorithm
+# Time Complexity: O(n)
+class Solution:
+    def maxSubArray(self, nums):
+        max_sum = 0
+        result = -pow(10,4)
+        hasPositive = False
+        for i in range(len(nums)):
+            if(max_sum + nums[i] > 0):
+                hasPositive = True
+                max_sum += nums[i]
+            else:
+                max_sum = 0
+            result = max(result, max_sum)
+        if(not hasPositive):
+            return max(nums)
+        else:
+            return result
+
+if __name__ == '__main__':
+    s = Solution()
+    # print(s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+    # print(s.maxSubArray([-1]))
+    print(s.maxSubArray([5,4,-1,7,8]))

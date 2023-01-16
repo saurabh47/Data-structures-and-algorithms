8e1f9ce6d8426de1ac05c29885f7bef9a9e030be
mahesh jamdade <maheshmn121@gmail.com>
2023-01-15 22:45:24 -0500

    optimize problem 3

--- leetcode/problem_3.py
+++ leetcode/problem_3.py
@@ -1,18 +1,22 @@
 # Problem 3: Longest Substring Without Repeating Characters (Medium): https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-def lengthOfLongestSubstring(self,s):
-    count = 0;
-    back = 0
-    front = 0
-    array = []
-    while(front < len(s)):
-        if(s[front] not in array):
-            array.append(s[front])
-            front += 1
-            count = max(count, len(array))
-        else:
-            array.remove(s[back])
-            back += 1
-    return(max(count, len(array)))
+class Solution(object):
+    def lengthOfLongestSubstring(self,s):
+        count = 0
+        back = 0
+        front = 0
+        if(len(s)==0):
+            return count
+        while(front < len(s)):
+            string = s[back:front]
+            if(string.find(s[front]) == -1):
+                front += 1
+                count = max(count, len(string))
+            else:
+                back += 1
+        return count+1
 
-lengthOfLongestSubstring("abcabcbb")
+if __name__ == "__main__":
+    s = "abcabcbb"
+    print(Solution().lengthOfLongestSubstring(s)) # 3
+    

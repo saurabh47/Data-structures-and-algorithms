849a0cb3041911ec406df3444221b9464a42d27d
mahesh jamdade <maheshmn121@gmail.com>
2023-05-21 16:44:04 -0400

    add alternate solution

--- leetcode/bit-manipulation/problem_338.py
+++ leetcode/bit-manipulation/problem_338.py
@@ -26,3 +26,19 @@ if __name__ == '__main__':
 # mahesh@Maheshs-MacBook-Air-M1 bit-manipulation % python3 problem_338.py
 # [0, 1, 1, 2, 1, 2]
 # [0, 1, 1]
+
+
+
+# Solution using dynamic programming
+# We use the previous result to calculate the current result
+# For example, if we have calculated the result for 1, we can use that to calculate the result for 5
+class Solution2:
+    def countBits(self, n: int) -> List[int]:
+        result = []
+        for number in range(n+1):
+            if(number == 0):
+                result.append(0)
+            else:
+                offset = 1 << int(floor(log(number, 2)))
+                result.append(1 + result[number-offset])
+        return result

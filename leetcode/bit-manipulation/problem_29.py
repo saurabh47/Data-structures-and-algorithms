37a142ed99acfe67d66e3a3ce8c8eeb4d212134f
mahesh jamdade <maheshmn121@gmail.com>
2024-03-24 20:23:01 -0400

    add handrun

--- leetcode/bit-manipulation/problem_29.py
+++ leetcode/bit-manipulation/problem_29.py
@@ -9,6 +9,44 @@
 # so that we can reduce the time complexity to O(log(n))
 # e.g 1M/1 we can subtract 512K, 256K, 128K, 64K, 32K, 16K, 8K, 4K, 2K, 1K, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 from dividend and simultaneously add 512K, 256K, 128K, 64K, 32K, 16K, 8K, 4K, 2K, 1K, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 to quotient
 
+# handrun
+# 58/4 = 14
+# quotient = 0
+# dividend = 58
+# 4 * 2^0 = 4 < 58
+# 4 * 2^1 = 8 < 58
+# 4 * 2^2 = 16 < 58
+# 4 * 2^3 = 32 < 58 (add 1 << 3 to quotient)
+# 4 * 2^4 = 64 < 58 (False)
+
+# quotient = 8 # 8
+# dividend = 58 - 32 = 26
+
+# 4 * 2^0 = 4 < 26
+# 4 * 2^1 = 8 < 26
+# 4 * 2^2 = 16 < 26 (add 1 << 2 to quotient)
+# 4 * 2^3 = 32 < 26 (False)
+
+# quotient = 12  # 8 + 4
+# dividend = 26 - 16 = 10
+
+# 4 * 2^0 = 4 < 10
+# 4 * 2^1 = 8 < 10 (add 1 << 1 to quotient)
+# 4 * 2^2 = 16 < 10 (False)
+
+# quotient = 14 # 8 + 4 + 2
+# dividend = 10 - 8 = 2
+
+# 4 * 2^0 = 4 < 2 (False) 
+
+# quotient = 14 # 8 + 4 + 2
+# dividend = 2 - 2 = 0
+
+# return 14
+
+
+
+
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         sign = 1

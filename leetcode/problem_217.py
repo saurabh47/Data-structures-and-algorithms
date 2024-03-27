ed5b23a713b7c765f513daf740db62c8ddb12af0
mahesh jamdade <maheshmn121@gmail.com>
2024-03-27 10:36:15 -0400

    improved code

--- leetcode/problem_217.py
+++ leetcode/problem_217.py
@@ -1,18 +1,13 @@
 # Problem 217: Contains Duplicate (Easy): https://leetcode.com/problems/contains-duplicate/
-
 class Solution:
     def containsDuplicate(self, nums):
         freq = {}
-        containsDuplicate = False
-        for i in range(len(nums)):
-            num = nums[i]
-            if(num not in freq):
-                freq[num] = 1
+        for number in nums:
+            if(number in freq):
+                return True
             else:
-                freq[num]+=1
-                containsDuplicate = True
-                break
-        return containsDuplicate
+                freq[number] = 1
+        return False
 
 if __name__ == "__main__":
     nums = [1,2,3,1]

3f5dd0282bdbfdab633b5c759520957e50b5c512
mahesh jamdade <maheshmn121@gmail.com>
2024-05-10 15:54:43 -0400

    add recursive solution

--- leetcode/problem_7.py
+++ leetcode/problem_7.py
@@ -18,6 +18,22 @@ class Solution:
             return 0
         return reverse
 
+# Recursive solution
+class Solution2:
+    def reverse(self, x: int) -> int:
+        result = self.reversed(abs(x), 0)
+        print("result:", result)
+        if(result.bit_length() > 31):
+            return 0
+        if(x < 0):
+            result *= -1
+        return result
+
+    def reversed(self, x:int, result: int)->int:
+        if(x < 1):
+            return result
+        result = (result * 10) + (x % 10)
+        return self.reversed(x//10, result)
 
 if __name__ == "__main__":
     print(Solution().reverse(123))

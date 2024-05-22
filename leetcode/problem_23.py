1bb0930dbe143a2c148e5a74857a88c3ffaab100
mahesh jamdade <maheshmn121@gmail.com>
2024-05-21 21:24:18 -0400

    add alternate solution

--- leetcode/problem_23.py
+++ leetcode/problem_23.py
@@ -6,6 +6,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Time Complexity: O(NlogK) where N is the total number of elements in all the linked lists and K is the number of linked lists
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         min_heap = []
@@ -25,4 +27,28 @@ class Solution:
             else:
                 tail.next = ListNode(popped)
                 tail = tail.next
+        return result
+
+# Time Complexity: O(NlogN) where N is the total number of elements in all the linked lists
+class Solution2:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        result = []
+        for i in range(len(lists)):
+            head = lists[i]
+            while(head):
+                result.append(head.val)
+                head = head.next
+        arr = sorted(result)
+        tail = None
+        result = None
+        i = 0
+        while(i < len(arr)):
+            popped = arr[i]
+            if(result is None):
+                result = ListNode(popped)
+                tail = result
+            else:
+                tail.next = ListNode(popped)
+                tail = tail.next
+            i += 1
         return result

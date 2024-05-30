6cf1246b65db6cb9ec6cf8676f334af461762c86
mahesh jamdade <maheshmn121@gmail.com>
2024-05-30 13:15:27 -0400

    ierative approach

--- leetcode/problem_144.py
+++ leetcode/problem_144.py
@@ -20,3 +20,22 @@ class Solution:
         order = []
         preOrder(root)
         return order
+
+### Iteration
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if(root is None):
+            return result
+        stack = []
+        while(True):
+            while(root):
+                stack.append(root)
+                result.append(root.val)
+                root = root.left
+            if(not stack):
+                break
+            root = stack.pop()
+            root = root.right
+        return result

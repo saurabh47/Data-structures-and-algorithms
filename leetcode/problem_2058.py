f7d84cf46d72c67d05107aa67a95fd79d5d343ce
mahesh jamdade <maheshmn121@gmail.com>
2024-07-05 11:15:47 -0400

    optimized

--- leetcode/problem_2058.py
+++ leetcode/problem_2058.py
@@ -38,3 +38,41 @@ class Solution:
         else:
             min_distance = -1
         return [min_distance, max_distance]
+
+
+### Optimized by calculating min max in same iteration
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        first_cp = -1
+        prev_cp = -1
+        prev = head
+        start = prev.next
+        if(start.next is None):
+            return [-1, -1]
+        min_distance = 100000
+        max_distance = -1
+        # start index
+        index = 1
+        while(start.next):
+            # check if critical point
+            if((prev.val > start.val and start.next.val > start.val) or (prev.val < start.val and start.next.val < start.val)):
+                if(first_cp == -1):
+                    first_cp = index   
+                else:
+                    if(index - prev_cp < min_distance):
+                        min_distance = index - prev_cp
+                    max_distance = index - first_cp
+                prev_cp = index
+            index += 1
+            prev = start
+            start = start.next
+
+        if(prev_cp == -1 or min_distance == 100000):
+            min_distance = -1
+        return [min_distance, max_distance]

423ddc281dea8b4ccec98f3746da5347704dc82a
mahesh jamdade <maheshmn121@gmail.com>
2024-07-31 22:12:42 -0400

    add recursive solution

--- leetcode/problem_783.py
+++ leetcode/problem_783.py
@@ -21,3 +21,26 @@ class Solution:
             if(abs(curr - prev) < min_val):
                 min_val = abs(curr - prev)
         return min_val
+
+# recursive
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        prev ,min_val = None, 100000
+        def min_diff(root):
+            if(root is None):
+                return 
+            min_diff(root.left)
+            nonlocal prev, min_val
+            if(prev):
+                min_val = min((root.val - prev.val), min_val)
+            prev = root
+            min_diff(root.right)
+        min_diff(root)
+        return min_val

33605c66b625a4c102cb1ddcf03a685035a9df6e
mahesh jamdade <maheshmn121@gmail.com>
2024-09-14 09:18:13 -0400

    alternate solution

--- leetcode/problem_2419.py
+++ leetcode/problem_2419.py
@@ -16,3 +16,23 @@ class Solution:
                 res = 0
             max_fr = max(res, max_fr)
         return max_fr
+    
+
+### Optimized
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        res = 0
+        size = 0
+        curr_max = 0
+        for num in nums:
+            if(num > curr_max):
+                curr_max = num
+                size = 1
+                res = 0
+            elif(num == curr_max):
+                size += 1
+            else:
+                size = 0
+            res = max(size, res)
+        return res

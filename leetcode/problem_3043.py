dabb6d09d96700a570147927ce276a88f38d8fd0
mahesh jamdade <maheshmn121@gmail.com>
2024-09-24 09:21:09 -0400

    daily streak

---
+++ leetcode/problem_3043.py
@@ -0,0 +1,25 @@
+### Problem 3043. Longest Common Prefix (Medium): https://leetcode.com/problems/longest-common-prefix/
+
+### tags: array, hashtable, string, trie
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        prefixes = set()
+        
+        for num in arr2:
+            while(num != 0):
+                prefixes.add(num)
+                num //= 10
+
+        max_len = 0
+        for num in arr1:
+            if(num in prefixes):
+                max_len = max(max_len, len(str(num)))
+            else:
+                while(num != 0):
+                    if(num in prefixes):
+                        max_len = max(max_len, len(str(num)))
+                        break
+                    num //= 10
+        return max_len
+
+

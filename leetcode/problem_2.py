0f8eedc194dbff9ab589618be1d767382e7919cd
mahesh jamdade <maheshmn121@gmail.com>
2024-10-27 19:35:32 -0400

    new solution

--- leetcode/problem_2.py
+++ leetcode/problem_2.py
@@ -67,3 +67,46 @@ class Solution2:
             current.next = ListNode(carry)
             current = current.next
         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution3:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        result = None
+        tail = None
+        while(l1 and l2):
+            s = l1.val + l2.val + carry
+            carry = s // 10
+            s = s % 10
+            if(result is None):
+                result = ListNode(s)
+                tail = result
+            else:
+                tail.next = ListNode(s)
+                tail = tail.next
+            l1 = l1.next
+            l2 = l2.next
+        print("one or both ended: carry=", carry)
+        if(l1 and l2 is None):
+            while(l1):
+                s = l1.val + carry
+                carry = s // 10
+                tail.next = ListNode(s % 10)
+                tail = tail.next
+                l1 = l1.next
+
+        if(l2 and l1 is None):
+            while(l2):
+                s = l2.val + carry
+                carry = s // 10
+                tail.next = ListNode(s % 10)
+                tail = tail.next
+                l2 = l2.next
+        if(carry > 0):
+            tail.next = ListNode(carry)
+
+        return result

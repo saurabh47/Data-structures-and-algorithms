ac83574a2ff21c7eb0c555eab1bff163976347a9
mahesh jamdade <maheshmn121@gmail.com>
2024-10-27 16:15:09 -0400

    add optimized solution

--- leetcode/problem_128.py
+++ leetcode/problem_128.py
@@ -29,6 +29,44 @@ class Solution:
             result = temp.copy()
         return len(result)
 
+#  Time Complexity: O(nlogn)
+# space complexity: O(1)
+class Solution2:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums.sort()
+        result = 0
+        count = 1
+        if(len(nums) == 0):
+            return 0
+        for i, num in enumerate(nums):
+            if((i != len(nums) - 1) and (num == nums[i + 1])):
+                continue
+            if((i != len(nums) - 1) and (num + 1 == nums[i + 1])):
+                count += 1
+            else:
+                result = max(result, count)
+                count = 1
+        result = max(result, count)
+        return result
+
+#  Time Complexity: O(n)
+# space complexity: O(n)
+class Solution3:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums)
+        count = 0
+        result = 0
+        for i, num in enumerate(s):
+            if(num - 1 not in s):
+                temp = num
+                count = 0
+                while(temp in s):
+                    count += 1
+                    temp += 1
+            result = max(result, count)
+        return result
+
 if __name__ == "__main__":
     nums = [1,2 ,0, 1]
     print(Solution().longestConsecutive(nums)) # 4
+

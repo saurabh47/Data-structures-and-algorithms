e9336260cf57f27afd3e29aa51791e33e59c301b
mahesh jamdade <maheshmn121@gmail.com>
2024-10-27 20:02:48 -0400

    add naive approach

--- leetcode/problem_4.py
+++ leetcode/problem_4.py
@@ -1,6 +1,32 @@
 # Problem 4: Median of Two Sorted Arrays (Hard): https://leetcode.com/problems/median-of-two-sorted-arrays/
 
+# Naive solution
 class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        result = []
+        i = 0
+        j = 0
+        while(i < len(nums1) and j < len(nums2)):
+            if(nums1[i] < nums2[j]):
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1
+
+        while(i < len(nums1)):
+            result.append(nums1[i])
+            i += 1
+        while(j < len(nums2)):
+            result.append(nums2[j])
+            j += 1
+        length = len(result)
+        if length & 1:  
+            return result[length // 2] 
+        else: 
+            return (result[length // 2] + result[(length // 2) -1]) / 2
+
+class Solution2:
     def findMedianSortedArrays(self, nums1, nums2):
         len1 = len(nums1)
         len2 = len(nums2)

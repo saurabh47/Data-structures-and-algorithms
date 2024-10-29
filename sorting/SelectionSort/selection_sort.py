6a2e4aa71232f12db45e33dd8ccfcc236a11eba5
mahesh jamdade <maheshmn121@gmail.com>
2024-10-29 17:12:46 -0400

    selection sort

---
+++ sorting/SelectionSort/selection_sort.py
@@ -0,0 +1,20 @@
+def swap(i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        smallest = i
+        for j in range(i, len(arr)):
+            if(arr[j] < arr[smallest]):
+                smallest = j
+        swap(smallest, i)
+        print(arr)
+    print("sorted arr=", arr)
+
+print("Enter the number of elements in the array")
+n = int(input())
+print("Enter the elements of the array")
+arr = list(map(int, input().split()))
+selection_sort(arr)

f4be9be57f00fb67128a866622e52ebe7e1b1bfb
mahesh jamdade <maheshmn121@gmail.com>
2024-11-03 07:56:59 -0500

    optimized solution

--- leetcode/problem_796.py
+++ leetcode/problem_796.py
@@ -21,3 +21,13 @@ class Solution2:
             if(a == goal):
                 return True
         return False
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        i = 0
+        while(i < len(s)):
+            s = s[1:] + s[0]
+            if(s==goal):
+                break
+            i+=1
+        return s==goal

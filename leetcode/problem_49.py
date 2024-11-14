11f72629c3a2edcfd72c270cd7ec5ba7835b91cf
mahesh jamdade <maheshmn121@gmail.com>
2024-11-14 10:44:30 -0500

    add alternate solution

--- leetcode/problem_49.py
+++ leetcode/problem_49.py
@@ -13,6 +13,22 @@ class Solution:
                 result[sortedWord].append(word)
         return list(result.values())
 
+# hint: sort each character and map to a key
+class Solution2:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        lookUp = {}    
+        result = []
+        for i, s in enumerate(strs):
+            sorted_s = ''.join(sorted(s, key=lambda x: ord(x)))
+            if(sorted_s in lookUp):
+                lookUp[sorted_s].append(s)
+            else:
+                lookUp[sorted_s] = [s]
+        for key, value in lookUp.items():
+            result.append(value)
+        return result
+
+
 if __name__ == "__main__":
     strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
     print(Solution().groupAnagrams(strs)) # [["ate","eat","tea"],["nat","tan"],["bat"]]

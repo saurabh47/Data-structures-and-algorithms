afbfac9ab36e4eba7b9b9a8349397c5aa1fa8170
mahesh jamdade <maheshmn121@gmail.com>
2024-11-14 10:19:48 -0500

    alternate solution

--- leetcode/problem_1.py
+++ leetcode/problem_1.py
@@ -1,5 +1,6 @@
 # Problem 1: Two Sum (Easy): https://leetcode.com/problems/two-sum/
 
+# Time Complexity O(n) memory: O(n)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         diffMap = {}
@@ -9,6 +10,22 @@ class Solution:
                 diffMap[nums[i]] = i
             else:
                 return [i, diffMap[diff]]
+
+# Time Complexity O(nlogn) memory: O(1)
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        print(nums)
+        start = 0
+        end = len(nums) - 1
+        while(start < end):
+            if(nums[start] + nums[end] == target):
+                return [start, end]
+            elif(nums[start] + nums[end] < target):
+                start += 1
+            else:
+                end -= 1
+        return []
 
 if __name__ == "__main__":
     nums = [2,7,11,15]

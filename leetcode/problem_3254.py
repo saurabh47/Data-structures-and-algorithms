8863f8b77cbf30d276164ca966acdde2e2912735
mahesh jamdade <maheshmn121@gmail.com>
2024-11-16 11:28:33 -0500

    add optimized soln

--- leetcode/problem_3254.py
+++ leetcode/problem_3254.py
@@ -1,5 +1,7 @@
 ### Problem 3254. Find the Power of K-Size Subarrays I
 ### tags: array, sliding window
+
+# hint1: maximum will be last element in the current window
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
     
@@ -23,3 +25,29 @@ class Solution:
             start += 1 
             end += 1
         return result
+
+# Optimized
+class Solution2:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+
+        start = 0
+        end = 0
+        result = []
+        cons_window_len = 1
+        while(end != len(nums)):
+            if(end > 0 and nums[end] - nums[end - 1] == 1):
+                cons_window_len += 1
+            
+            if(end - start + 1 > k):
+                if(nums[start + 1] - nums[start] == 1):
+                    cons_window_len -= 1
+                start += 1
+
+            if(end - start + 1 == k):
+                if(cons_window_len == k):
+                    result.append(nums[end])
+                else:
+                    result.append(-1)
+            end += 1
+        return result
+

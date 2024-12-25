75a428aa415fa5957b923d5e76977a444061ef3a
mahesh jamdade <maheshmn121@gmail.com>
2024-12-25 14:30:59 -0500

    add alternate solution using dfs

--- leetcode/problem_515.py
+++ leetcode/problem_515.py
@@ -35,3 +35,30 @@ class Solution:
                 size -=1
             result.append(maximum)
         return result
+
+# DFS:
+# # Time Complexity: O(N)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        # run dfs
+        # for first element at each level add to result
+        # for rest update the element by maxvalue at that level (index in result)
+        result = []
+        def dfs(r, depth):
+            if(r is None):
+                return 
+            if(depth >= len(result)):
+                result.append(r.val)
+            else:
+                result[depth] = max(result[depth], r.val)
+            dfs(r.left, depth + 1)
+            dfs(r.right, depth + 1)
+
+        dfs(root, 0)
+        return result

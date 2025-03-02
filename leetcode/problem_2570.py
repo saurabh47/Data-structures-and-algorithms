24bce111e8ec4ef5d09ea5d053f505648cef055f
mahesh jamdade <maheshmn121@gmail.com>
2025-03-02 09:52:30 -0500

    Optimized

--- leetcode/problem_2570.py
+++ leetcode/problem_2570.py
@@ -23,3 +23,33 @@ class Solution:
                     result.append([i, nm2[i]])
         return result
         
+        
+### Optimized Solution
+
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        result = [] 
+        i,j = 0, 0
+        while(i < len(nums1) and j < len(nums2)):
+            k1, v1 = nums1[i]
+            k2, v2 = nums2[j]
+            if(k1 < k2):
+                result.append([k1, v1])
+                i += 1
+            elif(k2 < k1):
+                result.append([k2, v2])
+                j += 1
+            else:
+                result.append([k2, v1 + v2])
+                i += 1
+                j += 1
+        while(i < len(nums1)):
+            k, v = nums1[i]
+            result.append([k, v])
+            i += 1
+        while(j < len(nums2)):
+            k, v = nums2[j]
+            result.append([k, v])
+            j += 1
+        return result
+        

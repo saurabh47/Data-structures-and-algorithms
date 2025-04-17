d27275e9a1479d540f20f2962020c601ad84ead5
mahesh jamdade <maheshmn121@gmail.com>
2025-04-17 08:23:49 -0400

    add dual implementations for finding the Kth largest element using max heap and min heap

--- leetcode/problem_215.kt
+++ leetcode/problem_215.kt
@@ -1,18 +1,47 @@
 // Problem 215: Kth Largest Element in an Array
 // tags: [Array, Divide and Conquer, Sorting, Heap (Priority Queue)]
+
+/*
+ * input: nums, n= 5
+ * output: kth largest
+ * Example: [1, 2, 3, 4, 2, 7, 6, 5 8, 10]
+ *        [1, 2, 2, 3, 4, 5, 6, 7, 8, 10]
+ *        [1, 2, 2, 3, 4, 5, 6, 7, 8, 10]
+ */
 class Solution {
     fun findKthLargest(nums: IntArray, k: Int): Int {
-        /*
-         * input: nums, n= 5
-         * output: kth largest
-         * Example: [1, 2, 3, 4, 2, 7, 6, 5 8, 10]
-         *        [1, 2, 2, 3, 4, 5, 6, 7, 8, 10]
-         *        [1, 2, 2, 3, 4, 5, 6, 7, 8, 10]
-         */
-        val minHeap = PriorityQueue<Int>()
+        val maxHeap = PriorityQueue<Int>(compareByDescending { it })
         for (num in nums) {
+            maxHeap.add(num)
+        }
+        var i = k
+        while (i != 1) {
+            maxHeap.poll()
+            i -= 1
+        }
+        return maxHeap.poll()
+    }
+}
+
+/**
+ * Min heap
+ * time complexity: O(nlog(k)) k <= n
+ * space complexity: O(k) k is the size of the heap
+ *  */
+
+class Solution {
+    fun findKthLargest(nums: IntArray, k: Int): Int {
+      /*
+       * input: nums, n= 5
+       * output: kth largest
+       * Example: [1, 2, 3, 4, 2, 7, 6, 5 8, 10] 
+       *        [1, 2, 2, 3, 4, 5, 6, 7, 8, 10]
+       *        [1, 2, 2, 3, 4, 5, 6, 7, 8, 10]
+       */
+        val minHeap = PriorityQueue<Int>()
+        for(num in nums){
             minHeap.add(num)
-            if (minHeap.size > k) {
+            if(minHeap.size > k){
                 minHeap.poll()
             }
         }

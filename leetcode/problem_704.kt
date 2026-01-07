942129fe82ec910f7819080fc09a84d917f18add
mahesh jamdade <maheshmn121@gmail.com>
2026-01-06 20:55:38 -0500

    binary search

--- leetcode/problem_704.kt
+++ leetcode/problem_704.kt
@@ -3,24 +3,19 @@
  */
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-        /**
-        * given: search target in sorted array
-        * input: arr
-        * output: index of target or -1
-        */
+        /** given: search target in sorted array input: arr output: index of target or -1 */
         var start = 0
         var end = nums.size - 1
-        while(start <= end){
+        while (start <= end) {
             var mid = start + (end - start) / 2
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid
-            }
-            else if(nums[mid] > target){
+            } else if (nums[mid] > target) {
                 end = mid - 1
             } else {
                 start = mid + 1
             }
         }
-        return -1    
+        return -1
     }
 }

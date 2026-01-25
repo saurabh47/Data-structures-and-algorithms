f142fd47837daa1495a01f7276515c6f67bac217
mahesh jamdade <maheshmn121@gmail.com>
2026-01-24 19:26:17 -0500

    refactor maxArea function in Container With Most Water solution for clarity and efficiency

--- leetcode/problem_11.py
+++ leetcode/problem_11.py
@@ -1,21 +1,36 @@
 # Problem 11: Container With Most Water (Medium): https://leetcode.com/problems/container-with-most-water/
 
+from typing import List
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
-    def maxArea(self, height) -> int:
-        max_area= 0 
+    def maxArea(self, height: List[int]) -> int:
+        # Approach
+
+        # Given
+        # - Array: Heights of bars at i
+
+        # Goal
+        # - find maximum container area
+        # - find two largest bars at longest distance apart
+        # - area = min(bar1, bar2) * dist(bar1, bar2)
+        # - compare the area with largest we currently have
+        # - return the largest
+
         start = 0
         end = len(height) - 1
+        max_water = 0
         while(start < end):
+            h = min(height[start], height[end])
             width = end - start
-            min_height = min(height[start], height[end])
-            area = width*min_height
-            if(area > max_area):
-                max_area = area
+            area = h * width
+            max_water = max(area, max_water)
             if(height[start] < height[end]):
-                start+=1
+                start += 1
             else:
-                end-=1
-        return max_area
+                end -= 1
+        return max_water
 
 
 if __name__ == "__main__":
